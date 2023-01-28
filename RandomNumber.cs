03c0f2438fae6123628b7f8c110f41fd5f980aed
ananyareddy2707 <115121894+ananyareddy2707@users.noreply.github.com>
2023-01-28 11:02:05 -0500

    newclassfile

---
+++ RandomNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class GetValue
+{
+    public string name { get; } = "Bat";
+}
+class RandomNumber
+{
+    private string ran;
+    public RandomNumber()
+    {
+        
+    }
+    static void MyMethod(string myString)
+            {
+                Random rnd = new Random();
+                int rndNum = rnd.Next(1, 100);
+                Console.WriteLine(rndNum + myString);
+            }
+    static void Main(string[] args)
+    { 
+        MyMethod("Ananya");
+        GetValue val = new GetValue();
+        Console.WriteLine(val.name);
+    }
+}
